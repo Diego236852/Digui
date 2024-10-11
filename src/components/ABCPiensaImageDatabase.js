09bf26a1a453919daf0b438c9e0ac4f185805c7a
Francisco Jarquin <frankharkin9181@gmail.com>
2024-10-11 10:12:40 -0600

    Ordenar los imports y lista de imagenes de forma alfabetica

--- src/components/ABCPiensaImageDatabase.js
+++ src/components/ABCPiensaImageDatabase.js
@@ -1,60 +1,60 @@
-import zapato from './../images/ABCPiensa/zapato.svg';
-import yoyo from './../images/ABCPiensa/yoyo.svg';
-import xilofono from './../images/ABCPiensa/xilofono.svg';
-import wifi from './../images/ABCPiensa/wifi.svg';
-import uva from './../images/ABCPiensa/uva.svg';
-import tuerca from './../images/ABCPiensa/tuerca.svg';
-import sopa from './../images/ABCPiensa/sopa.svg';
-import reloj from './../images/ABCPiensa/reloj.svg';
-import queso from './../images/ABCPiensa/queso.svg';
-import pera from './../images/ABCPiensa/pera.svg';
-import ojo from './../images/ABCPiensa/ojo.svg';
-import ñame from './../images/ABCPiensa/ñame.svg';
-import nube from './../images/ABCPiensa/nube.svg';
-import mochila from './../images/ABCPiensa/mochila.svg';
-import luna from './../images/ABCPiensa/luna.svg';
-import kilo from './../images/ABCPiensa/kilo.svg';
-import jarron from './../images/ABCPiensa/jarron.svg';
-import iman from './../images/ABCPiensa/iman.svg';
-import hoja from './../images/ABCPiensa/hoja.svg';
-import galleta from './../images/ABCPiensa/galleta.svg';
-import flor from './../images/ABCPiensa/flor.svg';
-import estrella from './../images/ABCPiensa/estrella.svg';
-import dedo from './../images/ABCPiensa/dedo.svg';
-import casa from './../images/ABCPiensa/casa.svg';
-import billete from './../images/ABCPiensa/billete.svg';
 import arbol from './../images/ABCPiensa/arbol.svg';
+import billete from './../images/ABCPiensa/billete.svg';
+import casa from './../images/ABCPiensa/casa.svg';
+import dedo from './../images/ABCPiensa/dedo.svg';
+import estrella from './../images/ABCPiensa/estrella.svg';
+import flor from './../images/ABCPiensa/flor.svg';
+import galleta from './../images/ABCPiensa/galleta.svg';
+import hoja from './../images/ABCPiensa/hoja.svg';
+import iman from './../images/ABCPiensa/iman.svg';
+import jarron from './../images/ABCPiensa/jarron.svg';
+import kilo from './../images/ABCPiensa/kilo.svg';
+import luna from './../images/ABCPiensa/luna.svg';
+import mochila from './../images/ABCPiensa/mochila.svg';
+import nube from './../images/ABCPiensa/nube.svg';
+import ñame from './../images/ABCPiensa/ñame.svg';
+import ojo from './../images/ABCPiensa/ojo.svg';
+import pera from './../images/ABCPiensa/pera.svg';
+import queso from './../images/ABCPiensa/queso.svg';
+import reloj from './../images/ABCPiensa/reloj.svg';
+import sopa from './../images/ABCPiensa/sopa.svg';
+import tuerca from './../images/ABCPiensa/tuerca.svg';
+import uva from './../images/ABCPiensa/uva.svg';
 import vaso from './../images/ABCPiensa/vaso.svg';
+import wifi from './../images/ABCPiensa/wifi.svg';
+import xilofono from './../images/ABCPiensa/xilofono.svg';
+import yoyo from './../images/ABCPiensa/yoyo.svg';
+import zapato from './../images/ABCPiensa/zapato.svg';
 
 // Poner las imágenes en un arreglo de objetos con su respectiva letra
 const imageDatabase = [
-  { src: zapato, letter: 'Z' },
-  { src: yoyo, letter: 'Y' },
-  { src: xilofono, letter: 'X' },
-  { src: wifi, letter: 'W' },
-  { src: uva, letter: 'U' },
-  { src: tuerca, letter: 'T' },
-  { src: sopa, letter: 'S' },
-  { src: reloj, letter: 'R' },
-  { src: queso, letter: 'Q' },
-  { src: pera, letter: 'P' },
-  { src: ojo, letter: 'O' },
-  { src: ñame, letter: 'Ñ' },
-  { src: nube, letter: 'N' },
-  { src: mochila, letter: 'M' },
-  { src: luna, letter: 'L' },
-  { src: kilo, letter: 'K' },
-  { src: jarron, letter: 'J' },
-  { src: iman, letter: 'I' },
-  { src: hoja, letter: 'H' },
-  { src: galleta, letter: 'G' },
-  { src: flor, letter: 'F' },
-  { src: estrella, letter: 'E' },
-  { src: dedo, letter: 'D' },
-  { src: casa, letter: 'C' },
-  { src: billete, letter: 'B' },
   { src: arbol, letter: 'A' },
-  { src: vaso, letter: 'V' }
+  { src: billete, letter: 'B' },
+  { src: casa, letter: 'C' },
+  { src: dedo, letter: 'D' },
+  { src: estrella, letter: 'E' },
+  { src: flor, letter: 'F' },
+  { src: galleta, letter: 'G' },
+  { src: hoja, letter: 'H' },
+  { src: iman, letter: 'I' },
+  { src: jarron, letter: 'J' },
+  { src: kilo, letter: 'K' },
+  { src: luna, letter: 'L' },
+  { src: mochila, letter: 'M' },
+  { src: nube, letter: 'N' },
+  { src: ñame, letter: 'Ñ' },
+  { src: ojo, letter: 'O' },
+  { src: pera, letter: 'P' },
+  { src: queso, letter: 'Q' },
+  { src: reloj, letter: 'R' },
+  { src: sopa, letter: 'S' },
+  { src: tuerca, letter: 'T' },
+  { src: uva, letter: 'U' },
+  { src: vaso, letter: 'V' },
+  { src: wifi, letter: 'W' },
+  { src: xilofono, letter: 'X' },
+  { src: yoyo, letter: 'Y' },
+  { src: zapato, letter: 'Z' }
 ];
 
 // Exportar la base de datos de imágenes
